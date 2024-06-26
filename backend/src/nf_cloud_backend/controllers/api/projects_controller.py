eb7fdb70c6728a5212fa5b583bef77c477be9d92
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2024-06-26 11:01:58 +0200

    fix is_directory call

--- backend/src/nf_cloud_backend/controllers/api/projects_controller.py
+++ backend/src/nf_cloud_backend/controllers/api/projects_controller.py
@@ -205,7 +205,7 @@ class ProjectsController:
         directory = project.get_path(
             Path(unquote(request.args.get('dir', "/", type=str)))
         )
-        if directory.is_dir() and project.in_file_director:
+        if directory.is_dir() and project.in_file_director(directory):
             files = []
             folders = []
             for entry in directory.iterdir():

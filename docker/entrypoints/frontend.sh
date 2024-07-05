9d82f2f1fb4fe2e464f41442145dd5758c835cd7
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2024-07-05 13:34:12 +0200

    add missing settings

--- docker/entrypoints/frontend.sh
+++ docker/entrypoints/frontend.sh
@@ -2,8 +2,10 @@
 
 # Write settings to file
 echo "NF_CLOUD_BACKEND_BASE_URL=$NF_CLOUD_BACKEND_BASE_URL" > current_settings.env
+echo "NF_CLOUD_BACKEND_WS_URL=$NF_CLOUD_BACKEND_WS_URL" > current_settings.env
 echo "NF_CLOUD_FRONTEND_INTERFACE=$NF_CLOUD_FRONTEND_INTERFACE" >> current_settings.env
 echo "NF_CLOUD_FRONTEND_PORT=$NF_CLOUD_FRONTEND_PORT" >> current_settings.env
+echo "NF_CLOUD_UPLOAD_MAX_FILE_SITE=$NF_CLOUD_UPLOAD_MAX_FILE_SITE" >> current_settings.env
 
 # Check if there is an file with old settings if not create an empty one
 if [ ! -f last_settings.env ]

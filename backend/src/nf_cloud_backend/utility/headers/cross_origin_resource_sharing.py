dff237f6d916488feacbdd7f1acf86f785ca4ff6
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2024-10-23 18:48:06 +0200

    formatting

--- backend/src/nf_cloud_backend/utility/headers/cross_origin_resource_sharing.py
+++ backend/src/nf_cloud_backend/utility/headers/cross_origin_resource_sharing.py
@@ -2,6 +2,6 @@ def add_allow_cors_headers(response):
     """
     Adds CORS headers
     """
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return response

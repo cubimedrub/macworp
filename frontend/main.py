71ec01796b89660d298b963d22852904e48b47fb
jona-S-sys <jonab97@web.de>
2025-07-14 19:32:46 +0200

    added settings

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,23 @@
 from nicegui import ui
 
-ui.label('Hello NiceGUI!')
+from config.routes import setup_routes
+from config.settings import Settings
 
-ui.run()
+
+def create_app():
+    settings = Settings()
+
+    setup_routes()
+
+    ui.run(
+        host=settings.host,
+        port=settings.port,
+        title=settings.app_name,
+        dark=settings.dark_mode,
+        reload=True,
+        show=False
+    )
+
+
+if __name__ == "__main__":
+    create_app()

6102ffc83fa275543db760bcfe598297390b6e68
jona-S-sys <jonab97@web.de>
2025-07-29 15:27:01 +0200

    added route for project edit

--- frontend/config/routes.py
+++ frontend/config/routes.py
@@ -1,9 +1,10 @@
-from nicegui import ui
+from nicegui import ui, app
 
 from ..components.common.header import render_header
 from ..pages import login, workflows, docs, dashboard
 from ..pages.login import LoginPage
-from ..pages.projects import projects_index, projects_new
+from ..pages.projects import projects_index, projects_new, projects_edit
+from nicegui import context
 
 
 def setup_routes():
@@ -20,8 +21,11 @@ def setup_routes():
 
     @ui.page('/projects')
     async def projects_page():
+        if not app.storage.user.get('authenticated', False):
+            ui.navigate.to('/')
+            return
         render_header()
-        projects = projects_index.ProjectPage()
+        projects = projects_index.ProjectsIndex()
         await projects.show()
 
     @ui.page('/projects/new')
@@ -30,12 +34,32 @@ def setup_routes():
         new_projects = projects_new.ProjectPageNew()
         return new_projects.show()
 
-    @ui.page('/workflows')
-    def workflows_page():
+    @ui.page('/projects/edit')
+    async def projects_page_edit():
         render_header()
-        return workflows.show()
+        project_id = None
+        try:
+            if hasattr(context, 'client') and hasattr(context.client, 'request'):
+                request = context.client.request
+                project_id = request.query_params.get('id')
+        except:
+            pass
+        if not project_id:
+            ui.label("project required").classes("test-danger")
+            return None
 
-    @ui.page('/docs')
-    def docs_page():
-        render_header()
-        return docs.show()
+        edit_projects = projects_edit.ProjectPageEdit(project_id)
+
+        await edit_projects.show()
+
+
+@ui.page('/workflows')
+def workflows_page():
+    render_header()
+    return workflows.show()
+
+
+@ui.page('/docs')
+def docs_page():
+    render_header()
+    return docs.show()

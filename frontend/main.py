240027511e0b4648f7fc2daa10aeff5ecbdf8281
jona-S-sys <jonab97@web.de>
2025-08-02 09:53:27 +0200

    added configs for test and result loading

--- frontend/main.py
+++ frontend/main.py
@@ -1,12 +1,18 @@
-from nicegui import ui
+import os
 
+from nicegui import app, ui
 from .config.settings import Settings
 from .config.routes import setup_routes
+
 
 def create_app():
     settings = Settings()
 
     setup_routes()
+
+    static_dir = os.path.abspath('projects')
+    app.add_static_files('/projects', static_dir)
+
     ui.run(
         storage_secret="lol",
         host=settings.host,
@@ -17,6 +23,6 @@ def create_app():
         show=False
     )
 
-if __name__ in {"__main__", "__mp_main__"}:
 
+if __name__ in {"__main__", "__mp_main__"}:
     create_app()
